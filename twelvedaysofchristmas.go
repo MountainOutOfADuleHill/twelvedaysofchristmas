290bad57137cf8e3ce69679c466e1979c2f46f5c
Noah <noahwilmott@yahoo.com>
2017-02-09 22:00:10 -0600

    Bonuses completed

--- twelvedaysofchristmas.go
+++ twelvedaysofchristmas.go
@@ -1,26 +1,36 @@
 /*
 December 19th, 2016 /r/DailyProgrammer challenge
 Difficulty: Easy
-No bonuses
+Bonuses 1 & 2 Completed
 */
 
 package main
 
 import (
   "fmt"
+  "bufio"
+  "os"
 )
 
 func main(){
+  reader := bufio.NewReader(os.Stdin)
   day := [12]string{"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"}
-  gift := [12]string{"Partridge in a Pear Tree", "Turtle Doves", "French Hens", "Calling Birds", "Golden Rings", "Geese a Laying",
-                  "Swans a Swimming", "Maids a Milking", "Ladies Dancing", "Lords a Leaping", "Pipers Piping", "Drummers Drumming"}
+  gift := [12]string{}
+  fmt.Println("Enter the 12 gifts to receive on Christmas: ")
+  for x, _ := range gift{
+    fmt.Print("Gift ", x+1, ": ")
+    gift[x], _ = reader.ReadString('\n')
+  }
+  fmt.Println("Entries Accepted.\n\n")
+  numString := [12]string{"a", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve"}
+
 
   for x, theDay := range day{//cycle through all the days
     fmt.Println("On the " + theDay + " day of Christmas\nmy true love sent to me:")
     //start in the xth position of gift and work down to 0
     for i := x; i >= 0; i--{
       if i == 0 && x != 0{fmt.Print("and ")}
-      fmt.Println(i+1, gift[i])
+      fmt.Println(numString[i], gift[i])
     }
 
   }
